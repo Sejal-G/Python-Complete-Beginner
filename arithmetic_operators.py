5209f18b6b340b691b14f300ffa9a4b4f49df5af
Sejal-G <41776112+Sejal-G@users.noreply.github.com>
2021-05-31 15:52:37 +0530

    Create arithmetic_operators.py
    Arithmetic operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,67 @@
+#Arithmetic Operators:
+
+'''  
+1. // 
+integer division
+Output: only an integer
+ 
+''' 
+#Logical or Bitwise operators
+
+'''
+1. &     -> and (ampersand)
+2. &&
+3. |
+4. ||
+
+
+truth table:
+AND
+false && false = false
+false && true = false
+true && false = false
+true && true = true!!
+
+OR
+false || false = false
+false || true = true
+true || true = true
+true || false = true!!
+
+8 && 10 = 8
+8 || 10 = 10 
+
+H.W.
+1. 9 && 9
+2. 10 && 12
+3. 19 && 20
+4. 5 || 3
+5. 20 || 3
+6. 30 && 12
+7. 12 || 3
+8. 12 || 13
+9. 24 && 12
+10. 18 || 2
+
+Whenever you have same numbers
+
+ 
+   1000
+|| 1010
+--------
+   1010  
+--------
+
+
+3==4 and 4==4
+
+
+
+
+step 1. conversion of decomal numbers into binary numbers
+step 2. and
+11 == 1000 or 
+
+      3==8 or 4==4
+
+'''
